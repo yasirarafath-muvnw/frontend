d4a0a76b94b9a1ab511956ebf8a995da8e6d7d46
yasir <yasir.arafath.munvw@gmail.com>
2025-07-28 18:04:49 +0530

    endpoints

--- api/endpoints/index.js
+++ api/endpoints/index.js
@@ -1,7 +1,32 @@
 export const endpoints = {
+  auth: {
     signUp: "api/auth/signup",
     login: "api/auth/login",
-    postProfilePic: 'api/user/upload',
-    postProfileDetails: 'api/user',
-    GetUserProfile: (data) => `api/user/${data}`
-}
+  },
+
+  user: {
+    create: "api/user",
+    update: (id) => `api/user/${id}`,
+    delete: (id) => `api/user/${id}`,
+    get: (id) => `api/user/${id}`,
+    uploadProfilePic: "api/user/upload",
+  },
+
+  tasks: {
+    create: "api/tasks",
+    getAll: "api/tasks",
+    getById: (id) => `api/tasks/${id}`,
+    deleteById: (id) => `api/tasks/${id}`,
+    getByUser: (userId) => `api/tasks/user/${userId}`,
+    updateStatus: (id) => `api/tasks/${id}`,
+  },
+
+  projects: {
+    create: "api/projects",
+    getAll: "api/projects",
+    getById: (id) => `api/projects/${id}`,
+    deleteById: (id) => `api/projects/${id}`,
+    update: (id) => `api/projects/${id}`,
+    getByUser: (userId) => `api/projects/user/${userId}`,
+  }
+};
